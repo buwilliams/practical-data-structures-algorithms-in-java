01b50bf06673180e37b876fce63ef4b5ad5fa068
Buddy Williams <buddywilliams@gmail.com>
2017-02-07 11:05:13 -0500

    Added recursive binary search

--- src/algo/recursion/App.java
+++ src/algo/recursion/App.java
@@ -3,13 +3,21 @@ package algo.recursion;
 public class App {
 
 	public static void main(String[] args) {
-		reduceByOne(10);
+		//reduceByOne(10);
 		
 		int[] ary = new int[]{1, 2, 3, 4, 5};
-		System.out.println(recursiveLinearSearch(ary, 0, 1));
-		System.out.println(recursiveLinearSearch(ary, 0, 3));
-		System.out.println(recursiveLinearSearch(ary, 0, 5));
-		System.out.println(recursiveLinearSearch(ary, 0, 6));
+		
+		// Recursive Linear Search
+		//System.out.println(recursiveLinearSearch(ary, 0, 1));
+		//System.out.println(recursiveLinearSearch(ary, 0, 3));
+		//System.out.println(recursiveLinearSearch(ary, 0, 5));
+		//System.out.println(recursiveLinearSearch(ary, 0, 6));
+		
+		// Recursive Binary Search
+		System.out.println(recursiveBinarySearch(ary, 0, ary.length - 1, 1));
+		System.out.println(recursiveBinarySearch(ary, 0, ary.length - 1, 5));
+		System.out.println(recursiveBinarySearch(ary, 0, ary.length - 1, 3));
+		System.out.println(recursiveBinarySearch(ary, 0, ary.length - 1, 6));
 	}
 	
 	// Recursion uses the stack data structure
@@ -24,6 +32,16 @@ public class App {
 		if(i > a.length - 1) return -1;
 		else if(a[i] == x) return i;
 		else return recursiveLinearSearch(a, i + 1, x);
+	}
+	
+	public static int recursiveBinarySearch(int[] a, int p, int r, int x) {
+		if(p > r) return -1; // base case
+		
+		int q = (p + r) / 2;
+		
+		if(a[q] > x) return recursiveBinarySearch(a, p, q-1, x);
+		else if(a[q] < x) return recursiveBinarySearch(a, q+1, r, x);
+		else return q;
 	}
 
 }
--- src/algo/recursion/README.md
+++ src/algo/recursion/README.md
@@ -1,5 +1,7 @@
 # Recursive Linear Search
 
+## Recursive Linear Search
+
 Procedure: Recursive Linear Search (A, i, x):
 
 Inputs: Save as linearSearch but with added parameter i.
@@ -9,4 +11,20 @@ Output: The index of an element matching x in the subarray from A[i]
 Steps:
 1. If i > n then return -1
 2. else if a[i] == x then return i
-3. else recur(a, i+1, x)  
+3. else recur(a, i+1, x)
+
+## Recursive Binary Search
+
+Procedure: Recursive Binary Search
+
+Inputs: a = int[], p = start index, r = end index, x = search value 
+Outputs: returns p if a[p] == x  or -1 if value not found
+
+Steps:
+1. if p > r return -1 (base case)
+2. q = (p + r) / 2
+3. if a[q] > x then return recur(a, p, q-1, x)
+4. else if a[q] < x then return recur(a, p+1, r, x)
+5. else return p
+
+
